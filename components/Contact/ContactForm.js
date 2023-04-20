aa0a59c597e01b946fef2b56b582f996fe50d2c0
Manish Srivastav <srivastavmanish389@gmail.com>
2023-04-21 01:49:44 +0530

    Added the Functionality the Services We Provide
    Made the Functionality that Each User have to select the only one functionality at time

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,145 +1,183 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-const MySwal = withReactContent(Swal)
-import baseUrl from '../../utils/baseUrl'
-import ContactServices from './ContactServices'
-import ServicesWeProvide from './ServicesWeProvide'
+import React, { useState } from 'react';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+const MySwal = withReactContent(Swal);
+import baseUrl from '../../utils/baseUrl';
+import ContactServices from './ContactServices';
+import ServicesWeProvide from './ServicesWeProvide';
+import { BsFillFileTextFill } from 'react-icons/bs';
+import { BsRocketTakeoffFill } from 'react-icons/bs';
+import { MdCellWifi } from 'react-icons/md';
+
+const List = [
+  {
+    id: 1,
+    data: 'Startup/D2C Brand',
+    IconStyle: {
+      IconTitle: BsFillFileTextFill,
+    },
+  },
+
+  {
+    id: 2,
+    data: 'Existing/NewBuisness',
+    IconStyle: {
+      IconTitle: BsRocketTakeoffFill,
+    },
+  },
+
+  {
+    id: 3,
+    data: 'Product/Tech-Companies',
+    IconStyle: {
+      IconTitle: MdCellWifi,
+    },
+  },
+
+
+
+];
 
 const alertContent = () => {
-    MySwal.fire({
-        title: 'Congratulations!',
-        text: 'Your message was successfully send and will back to you soon',
-        icon: 'success',
-        timer: 2000,
-        timerProgressBar: true,
-        showConfirmButton: false,
-    })
-}
+  MySwal.fire({
+    title: 'Congratulations!',
+    text: 'Your message was successfully send and will back to you soon',
+    icon: 'success',
+    timer: 2000,
+    timerProgressBar: true,
+    showConfirmButton: false,
+  });
+};
 
 // Form initial state
 const INITIAL_STATE = {
-    name: "",
-    email: "",
-    number: "",
-    subject: "",
-    text: ""
+  name: '',
+  email: '',
+  number: '',
+  subject: '',
+  text: '',
 };
 
 const ContactForm = () => {
+  const [contact, setContact] = useState(INITIAL_STATE);
 
-    const [contact, setContact] = useState(INITIAL_STATE);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setContact((prevState) => ({ ...prevState, [name]: value }));
+    // console.log(contact)
+  };
 
-    const handleChange = e => {
-        const { name, value } = e.target;
-        setContact(prevState => ({ ...prevState, [name]: value }));
-        // console.log(contact)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = `${baseUrl}/api/contact`;
+      const { name, email, number, subject, text } = contact;
+      const payload = { name, email, number, subject, text };
+      const response = await axios.post(url, payload);
+      console.log(response);
+      setContact(INITIAL_STATE);
+      alertContent();
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const handleSubmit = async e => {
-        e.preventDefault();
-        try {
-            const url = `${baseUrl}/api/contact`;
-            const { name, email, number, subject, text } = contact;
-            const payload = { name, email, number, subject, text };
-            const response = await axios.post(url, payload);
-            console.log(response);
-            setContact(INITIAL_STATE);
-            alertContent();
-        } catch (error) {
-            console.log(error)
-        }
-    };
+  return (
+    <>
+      <div className="contact-form">
+        {/* <h2>Get In Touch</h2> */}
 
-    return (
-
-      <> 
-      
-        <div className="contact-form">
-            {/* <h2>Get In Touch</h2> */}
-  
-            <form onSubmit={handleSubmit}>
-
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div> Name  <span>*</span> </div>
-                             <br></br>
-                            <div className="form-group">
-                            
-                                <input 
-                                    type="text" 
-                                    name="name" 
-                                    placeholder="Name" 
-                                    className="form-control" 
-                                    value={contact.name}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         <br></br>
-
-                        <div className="col-lg-6">
-                             <div> Email  <span>*</span> </div>
-                              <br></br>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    name="email" 
-                                    placeholder="Email" 
-                                    className="form-control" 
-                                    value={contact.email}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         <br></br>
-                        <div className="col-lg-6">
-                             <div> Company Name  <span>*</span></div>
-                                                      <br></br>
-
-                            
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    name="number" 
-                                    placeholder="Phone number" 
-                                    className="form-control" 
-                                    value={contact.number}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         <br></br>
-                        <div className="col-lg-6">
-                             <div> Contact Number  <span>*</span> </div> 
-                              <br></br>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    name="subject" 
-                                    placeholder="Contact Number" 
-                                    className="form-control" 
-                                    value={contact.subject}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-
-                         <div  className='contactservices'> 
-                <ContactServices/>
+        <form onSubmit={handleSubmit}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Name <span>*</span>{' '}
                 </div>
-
-                 <div  className='contactservices'> 
-                <ServicesWeProvide/>
+                <br></br>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    className="form-control"
+                    value={contact.name}
+                    onChange={handleChange}
+                    required
+                  />
                 </div>
+              </div>
+              <br></br>
 
-                        {/* <div className="col-lg-12 col-md-12">
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Email <span>*</span>{' '}
+                </div>
+                <br></br>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    className="form-control"
+                    value={contact.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <br></br>
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Company Name <span>*</span>
+                </div>
+                <br></br>
+
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="number"
+                    placeholder="Phone number"
+                    className="form-control"
+                    value={contact.number}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <br></br>
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Contact Number <span>*</span>{' '}
+                </div>
+                <br></br>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Contact Number"
+                    className="form-control"
+                    value={contact.subject}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="contactservices">
+                <ContactServices  List={List} />
+              </div>
+
+              <div className="contactservices">
+                <ServicesWeProvide />
+              </div>
+
+              {/* <div className="col-lg-12 col-md-12">
                             <div className="form-group">
                                 <textarea 
                                     name="text" 
@@ -154,101 +192,105 @@ const ContactForm = () => {
                             </div>
                         </div> */}
 
-                        {/* <div className="col-lg-12 col-sm-12">
+              {/* <div className="col-lg-12 col-sm-12">
                             <button type="submit" className="btn btn-primary">
                                 Send Message
                             </button>
                         </div> */}
-                    </div>
+            </div>
+          </div>
+          <br></br>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Project Budget <span>*</span>{' '}
                 </div>
-                 <br></br>
+                <br></br>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder=" Our Project start at minimum 1.5 Lacs"
+                    className="form-control"
+                    value={contact.name}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <br></br>
 
-                 <div className="container">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <div> Project Budget  <span>*</span> </div>
-                             <br></br>
-                            <div className="form-group">
-                            
-                                <input 
-                                    type="text" 
-                                    name="name" 
-                                    placeholder=" Our Project start at minimum 1.5 Lacs" 
-                                    className="form-control" 
-                                    value={contact.name}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         <br></br>
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  Project DeadLine <span>*</span>{' '}
+                </div>
+                <br></br>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Tell us in in terms of month"
+                    className="form-control"
+                    value={contact.email}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <br></br>
 
-                        <div className="col-lg-6">
-                             <div> Project DeadLine <span>*</span> </div>
-                              <br></br>
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    name="email" 
-                                    placeholder="Tell us in in terms of month" 
-                                    className="form-control" 
-                                    value={contact.email}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         <br></br>
+              <br></br>
 
-                        
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  How did you hear about us <span>*</span>{' '}
+                </div>
+                <br></br>
+                <div className="form-group">
+                  <textarea
+                    type="text"
+                    name="Your Message"
+                    placeholder="Let us Know about Your Project"
+                    cols="30"
+                    rows="6"
+                    className="form-control"
+                    value={contact.subject}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
 
-                         <br></br>
+              <br></br>
 
-                        <div className="col-lg-6">
-                             <div> How did you hear about us <span>*</span> </div> 
-                              <br></br>
-                            <div className="form-group">
-                                <textarea 
-                                    type="text" 
-                                    name="Your Message" 
-                                    placeholder="Let us Know about Your Project" 
-                                    cols="30" 
-                                    rows="6" 
-                                    className="form-control" 
-                                    value={contact.subject}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-                         
-                         <br></br>
+              <div className="col-lg-6">
+                <div>
+                  {' '}
+                  How did you hear about us <span>*</span>
+                </div>
+                <br></br>
 
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="number"
+                    placeholder="Tell Your Sources"
+                    className="form-control"
+                    value={contact.number}
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
 
-                         <div className="col-lg-6">
-                             <div> How did you hear about us  <span>*</span></div>
-                                                      <br></br>
+              <br></br>
 
-                            
-                            <div className="form-group">
-                                <input 
-                                    type="text" 
-                                    name="number" 
-                                    placeholder="Tell Your Sources" 
-                                    className="form-control" 
-                                    value={contact.number}
-                                    onChange={handleChange} 
-                                    required 
-                                />
-                            </div>
-                        </div>
-
-                         <br></br>
-                          
-
-
-
-                        {/* <div className="col-lg-12 col-md-12">
+              {/* <div className="col-lg-12 col-md-12">
                             <div className="form-group">
                                 <textarea 
                                     name="text" 
@@ -263,23 +305,18 @@ const ContactForm = () => {
                             </div>
                         </div> */}
 
-                        <div className="col-lg-12 col-sm-12">
-                            <button type="submit" className="btn btn-primary">
-                                Connect With Us
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                 <br></br>
-            </form>
-
-           
-        </div>
-
-
-
-        </>
-    )
-}
+              <div className="col-lg-12 col-sm-12">
+                <button type="submit" className="btn btn-primary">
+                  Connect With Us
+                </button>
+              </div>
+            </div>
+          </div>
+          <br></br>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default ContactForm;
--- components/Contact/ContactServices.js
+++ components/Contact/ContactServices.js
@@ -1,49 +1,147 @@
-import React from 'react';
-import { BsGrid3X3GapFill } from 'react-icons/bs';
+import React, { useState } from 'react';
 import { BsFillFileTextFill } from 'react-icons/bs';
 import { BsRocketTakeoffFill } from 'react-icons/bs';
 import { MdCellWifi } from 'react-icons/md';
 
+// import styled from '../../styles/contactservices.module.css';
+import styled from 'styled-components';
+import styles from"../../styles/changeidx.module.css"
+import "../../styles/changeidx.module.css"
 
 
 
+// const List = [
+//   {
+//     id: 1,
+//     data: 'Startup/D2C Brand',
+//     IconStyle: {
+//       IconTitle: <BsFillFileTextFill />,
+//     },
+//   },
+
+//   {
+//     id: 2,
+//     data: 'Existing/NewBuisness',
+//     IconStyle: {
+//       IconTitle: <BsFillFileTextFill />,
+//     },
+//   },
+
+//   {
+//     id: 3,
+//     data: 'Product/Tech-Companies',
+//     IconStyle: {
+//       IconTitle: <BsFillFileTextFill />,
+//     },
+//   },
+// ];
+
+
+  
+ 
 
 const ContactServices = () => {
+
+const [idx, setidx] = useState('initialstyle');
+
+ 
+
+
+  
+
+ // console.log(List[0].IconStyle.IconTitle);
+
+   
   return (
     <>
       <div className="contactServices">
-        
-        <h2>Services We Provide</h2> 
+        <h2>Services We Provide</h2>
       </div>
-
+      
       <div className="contactServicesbox">
 
-        <div className={`contactServicesboxdiv`} >
+        <div className={`contactServicesboxdiv`} id={idx} onClick={()=>setidx('b')}>
+          <BsFillFileTextFill size={50} />
+         <p>Startup/D2C Brand </p> 
 
-            {/* <div  className='reacticons'> <BsFillFileTextFill size={50}/>  </div> */}
-            <BsFillFileTextFill  size={50}/>  
-                   Startup/D2C Brand
+         <style jsx>
+          {`
+            #b{
+             border: 0.2rem double green;
+              border-radius: 2rem;
+              background-color: orange
 
 
-         {/* <div>  Startup/D2C </div> */}
+            }
+
+             p{
+              font-size: 0.75rem;
+            }
+
+          `}
+
+
+         </style>
+        </div>
+
+        <div className="contactServicesboxdiv" id={idx} onClick={()=>setidx('a')}>
+          <BsRocketTakeoffFill size={50} />
+        <p>   Existing/NewBuisness </p> 
+
+        <style jsx>
+          {`
+            #a{
+              border: 0.2rem double green;
+              border-radius: 2rem;
+              background-color: orange
+            }
+
+            p{
+              font-size: 0.75rem;
+            }
+
+          `}
+         </style>
+        
+        </div>
+
+        <div className="contactServicesboxdiv" id={idx} onClick={()=>setidx('c')} >
+          <MdCellWifi size={50} />
+         <p> 
+           Product/Tech-Companies </p>
+
+           <style jsx>
+          {`
+            #c{
+              border: 0.2rem double green;
+              border-radius: 2rem;
+              background-color: orange
+            }
+
+            p{
+              font-size: 0.75rem;
+            }
+
+          `}
+         </style>
 
         </div>
-        <div className="contactServicesboxdiv">
 
-           {/* <div  className='reacticons'> <HiRocketLaunch size={50}/>  </div> */}
-                       <BsRocketTakeoffFill size={50}/>  
-                         Existing/NewBuisness 
-
-
-        </div>
-        <div className="contactServicesboxdiv">
-
-           <MdCellWifi size={50}/>  
-            Product/Tech-Companies
-        </div>
       </div>
+{/* 
+      <div>
+        {List.map((item, idx) => (
+          <StyledItem color={idx === selectedIndex ? '#1bb953' : '#fff'}>
+            <div> {item.data} </div>
+            <div></div>
+          </StyledItem>
+        ))}
+      </div> */}
+
     </>
   );
 };
+
+
 
 export default ContactServices;
